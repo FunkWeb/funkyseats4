8365cf5bd707c31dc7c351dc8eb27908bb2f74a0
Marcus Milsem <47544415+Marmil321@users.noreply.github.com>
2026-09-22 12:06:45 +0200

    build(deps): limit typescript version; upgrade ESLint and config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,11 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
 
-export default tseslint.config(
+export default defineConfig(
+  globalIgnores(['dist']),
   js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'error',
-    },
-  },
+  tseslint.configs.recommended,
   prettier,
 )
