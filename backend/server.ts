640941c014b38c31d29f69ef4e21f66ecce36660
Marcus Milsem <47544415+Marmil321@users.noreply.github.com>
2026-09-22 11:43:56 +0200

    chore: run format script; add current md files to ignore file

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
-import { app } from "./app.js";
+import { app } from './app.js'
 
-const port = Number(process.env.PORT ?? 3000);
+const port = Number(process.env.PORT ?? 3000)
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+  console.log(`http://localhost:${port}`)
+})
